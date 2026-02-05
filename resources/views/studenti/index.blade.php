f46d0feddb0b301bc3561445c72f023e44a6b444
mkrivic83 <krivic.milan@gmail.com>
2026-02-05 21:05:37 +0100

    promjene na stranici studenti - vrijeme

--- resources/views/studenti/index.blade.php
+++ resources/views/studenti/index.blade.php
@@ -11,6 +11,7 @@
 
 <a class="btn" href="{{ route('studenti.create') }}">+ Novi student</a>
 
+<p>Trenutno vrijeme: {{ \Carbon\Carbon::now()->format('d.m.Y H:i:s') }}</p>
 <table style="margin-top: 1rem;">
   <tr>
     <th>ID</th>
